c8d47967057e64307e4696356a519f55e72d53ce
felipeS-034 <andressantacruz268@gmail.com>
2022-09-02 23:22:34 -0500

    Data Instagram.

---
+++ data.js
@@ -0,0 +1,125 @@
+const data= [   
+   {
+        
+        "imageprofile" : "./...",
+        "nameuser" : "kata_1303",
+        "location" : "",
+        "imagecontent" : "./...",
+        "likes" : "500 likes",
+        "comentuser1" : "kata_1303",
+        "comentuser2" : "holi",
+        "viewcoments" : "View all 7 comments",
+        "days" : "10 DAYS AGO"
+
+
+    },
+
+    {
+        "imageprofile" : "./...",
+        "nameuser" : "luisa.11:11",
+        "location" : "",
+        "imagecontent" : "./...",
+        "likes" : "500 likes",
+        "comentuser1" : "luisa.11:11",
+        "comentuser2" : "holi",
+        "viewcoments" : "View all 5 comments",
+        "days" : "11 DAYS AGO"
+    },
+
+    {
+        "imageprofile" : "./...",
+        "nameuser" : "Richi-jeje",
+        "location" : "",
+        "imagecontent" : "./...",
+        "likes" : "500 likes",
+        "comentuser1" : "Richi-jeje",
+        "comentuser2" : "program es lo mio",
+        "viewcoments" : "View all 5 comments",
+        "days" : "13 DAYS AGO"
+    },
+
+    {
+        "imageprofile" : "./...",
+        "nameuser" : "isabella.bogota",
+        "location" : "",
+        "imagecontent" : "./...",
+        "likes" : "500 likes",
+        "comentuser1" : "isabella.bogota",
+        "comentuser2" : "holi",
+        "viewcoments" : "View all 3 comments",
+        "days" : "15 DAYS AGO"
+    },
+
+    {
+        "imageprofile" : "./...",
+        "nameuser" : "andrea_d75",
+        "location" : "",
+        "imagecontent" : "./...",
+        "likes" : "500 likes",
+        "comentuser1" : "andrea_d75",
+        "comentuser2" : "holi",
+        "viewcoments" : "View all 1 comments",
+        "days" : "14 DAYS AGO"
+    },
+
+    {
+        "imageprofile" : "./...",
+        "nameuser" : "sara_acosta.g9",
+        "location" : "",
+        "imagecontent" : "./...",
+        "likes" : "500 likes",
+        "comentuser1" : "sara_acosta.g9",
+        "comentuser2" : "holi",
+        "viewcoments" : "View all 7 comments",
+        "days" : "13 DAYS AGO"
+    },
+
+    {
+        "imageprofile" : "././...",
+        "nameuser" : "imsararozog_",
+        "location" : "",
+        "imagecontent" : "./...",
+        "likes" : "500 likes",
+        "comentuser1" : "imsararozog_",
+        "comentuser2" : "no somos ni seremos",
+        "viewcoments" : "View all 6 comments",
+        "days" : "45 DAYS AGO"
+    },
+
+    {
+        "imageprofile" : "./...",
+        "nameuser" : "avitery17",
+        "location" : "",
+        "imagecontent" : "./...",
+        "likes" : "500 likes",
+        "comentuser1" : "avitery17",
+        "comentuser2" : "broo",
+        "viewcoments" : "View all 5 comments",
+        "days" : "4 DAYS AGO"
+    },
+
+    {
+        "imageprofile" : "./...",
+        "nameuser" : "sara_rozo_23",
+        "location" : "",
+        "imagecontent" : "./...",
+        "likes" : "500 likes",
+        "comentuser1" : "sara_rozo_23",
+        "comentuser2" : "tampoco somos ni seremos",
+        "viewcoments" : "View all 5 comments",
+        "days" : "7 DAYS AGO"
+    },
+
+    {
+        "imageprofile" : "./image/me.jpg",
+        "nameuser" : "andres_santacruz03",
+        "location" : "",
+        "imagecontent" : "./...",
+        "likes" : "500 likes",
+        "comentuser1" : "andres_santacruz03",
+        "comentuser2" : "I'm the best",
+        "viewcoments" : "View all 600K comments",
+        "days" : "1 DAYS AGO"
+    }
+
+]
